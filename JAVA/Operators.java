74833943677b2b6560bcd8bb670e6b949757bd02
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-09-22 00:03:25 +0900

    Create Operators.java

---
+++ JAVA/Operators.java
@@ -0,0 +1,32 @@
+public class Operators {
+    public static void main(String[] args) {
+    Operators sample=new Operators();
+    sample.additive();
+    sample.multiplicative();
+    sample.remainder();
+    }
+    public void additive() {
+        int intValue1=5;
+        int intValue2=10;
+        int result=intValue1 + intValue2;
+        System.out.println(result);
+        result=intValue2-intValue1;
+        System.out.println(result);
+    }
+    public void multiplicative() {
+        int intValue1 = 5;
+        int intValue2= 10;
+        
+        int result =intValue1 * intValue2;
+        System.out.println(result);
+        result=intValue2/intValue1;
+        System.out.println(result);
+    }
+    public void remainder() {
+        int intValue1=53;
+        int intValue2=10;
+        int result=intValue1 % intValue2;
+        System.out.println(result);
+    }
+    
+}
