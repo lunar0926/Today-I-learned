eed859f42fd74e0f97412a1764544a6bb2cedffd
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-09-22 00:03:46 +0900

    Create ReferenceString.java

---
+++ JAVA/ReferenceString.java
@@ -0,0 +1,7 @@
+public class ReferenceString {
+    public ReferenceString() {}
+    public ReferenceString(String arg) {}
+    public static void main(String [] args) {
+        ReferenceString reference = new ReferenceString();
+    }
+}
