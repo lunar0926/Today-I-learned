b56cec60cdcf60e6efbf2077b90e9db78fe63cab
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-09-24 00:35:21 +0900

    Create AccessModifier.java

---
+++ JAVA/c/javapackage/sub/AccessModifier.java
@@ -0,0 +1,12 @@
+package c.javapackage.sub;
+
+public class AccessModifier {
+  public void publicMethod() {
+  }
+  protected void  protectedMethod() {
+  }
+  void packagePrivateMethod() {
+  }
+  private void privateMethod() {
+  }
+}
