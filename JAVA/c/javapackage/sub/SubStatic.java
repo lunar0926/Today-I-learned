1b3d243aaf3a1226bdfc1c6c48ee667f172cbb6d
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-09-24 00:35:25 +0900

    Create SubStatic.java

---
+++ JAVA/c/javapackage/sub/SubStatic.java
@@ -0,0 +1,10 @@
+package c.javapackage.sub;
+
+public class SubStatic {
+  public final static String CLASS_NAME="SubStatic";
+  public static void subStaticMethod() {
+    System.out.println("subStaticmethod() is called.";
+  }
+
+} 
+// static 메소드와 클래스 변수
