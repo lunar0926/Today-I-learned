7c5926749fd9d31d59dd5de67ddd623b18ee8518
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-10-08 17:39:23 +0900

    Create Problem3.java

---
+++ JAVA/ch6/src/ch6/Problem3.java
@@ -0,0 +1,16 @@
+package ch6;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		for (int x = 0; x <= 10; x++) {
+			for(int y = 0; y <= 10; y++) {
+				if (3 * x + 10 * y == 100) {
+					System.out.println("("+x+","+y+")");
+				}
+			}
+		}
+
+	}
+
+}
