d6fce1f35a6b3d3e85c278ead36b482820c37659
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-11-19 00:08:33 +0900

    Create CircleTest.java

---
+++ JAVA/lab3/src/lab3/CircleTest.java
@@ -0,0 +1,9 @@
+package lab3;
+
+public class CircleTest {
+	public static void main(String[] args) {
+		Circle c = new Circle(5.0);
+		System.out.println(c.getArea());
+		System.out.println(c.getPerimeter());
+	}
+}
