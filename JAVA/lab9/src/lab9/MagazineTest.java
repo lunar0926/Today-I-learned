7df08df060e0af2da302772df075ad67c8577d6e
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-11-19 00:09:19 +0900

    Create MagazineTest.java

---
+++ JAVA/lab9/src/lab9/MagazineTest.java
@@ -0,0 +1,11 @@
+package lab9;
+
+public class MagazineTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Magazine m1 = new Magazine("Newsweek", 100, "aa", "2020-11-17");
+		System.out.println(m1);
+	}
+
+}
