15dff15ad3de5f27cc48ebfcc7dd8b589a3d1adc
lunar0926 <52964052+lunar0926@users.noreply.github.com>
2020-11-19 00:09:23 +0900

    Create TestCylinder.java

---
+++ JAVA/lab9/src/lab9/TestCylinder.java
@@ -0,0 +1,10 @@
+package lab9;
+
+public class TestCylinder {
+	public static void main(String[] args) {
+		Cylinder obj1 = new Cylinder();
+		System.out.println(obj1);
+		Cylinder obj2 = new Cylinder(5.0, 3.0);
+		System.out.println(obj2);
+	}
+}
